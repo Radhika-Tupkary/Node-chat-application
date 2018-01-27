374509b32b2d4ca99fe1f1f6ab0a062a377ec1f6
Radhika Tupkary <radhika@lucenaresearch.com>
2018-01-27 14:51:10 -0500

    created server and a client to establish two way connection

--- server/server.js
+++ server/server.js
@@ -1,17 +1,33 @@
 const path = require('path');
-const publicPath = path.join(__dirname, '../public');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 let app = express();
+
+const publicPath = path.join(__dirname, '../public');
+
+let server = http.createServer(app);
+let io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
 console.log(publicPath);
 console.log(__dirname + '/../public');
 
+io.on('connection', (socket) => {
+    console.log("New user connected");
+
+    socket.on('disconnect', () => {
+        console.log("User was disconnected");
+    });
+});
 
 
-app.listen(port, () => {
+
+
+
+server.listen(port, () => {
    console.log(`Server is running on port ${port}!`);
 });
